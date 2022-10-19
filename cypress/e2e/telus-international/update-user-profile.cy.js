24a47aca14544b2ad2316b12091ea1c78ec64bf9
zqarnain <zronsing@gmail.com>
2022-10-19 14:47:38 +0800

    Added steps for updating languages and signing-out from the application

--- cypress/e2e/telus-international/update-user-profile.cy.js
+++ cypress/e2e/telus-international/update-user-profile.cy.js
@@ -1,12 +1,46 @@
-context('Update User Profile', () => {
-    it('Invoke Telus-International URL', () => {
-        cy.visit('https://www.telusinternational.ai/cmp')
+describe('Update User Profile', () => {
+    let url, credentials, userProfile
+
+    beforeEach(function () {
+        cy.fixture('url').then(function (data) {
+            this.url = data
+        })
+
+        cy.fixture('credentials').then(function (data) {
+            this.credentials = data
+        })
+
+        cy.fixture('userProfile').then(function (data) {
+            this.userProfile = data
+        })
     })
 
+    describe('Invoke Telus-International AI URL', () => {
+        it('Launch Website', function () {
+            cy.visit(this.url.baseURL)
+
+            cy.wait(5000)
+        })
+
+
+        it('Validate Telus logo is displayed and has correct href link', () => {
+            cy.get('a[aria-label="logo"]').should('be.visible')
+
+            cy.get('a[aria-label="logo"]').should('have.attr', 'href').and('include', '/cmp/')
+        })
+
+        it('Validate Welcome back! text is displayed in the login page', () => {
+            cy.get('div[class="tw-col-start-2 tw-col-span-5 lg:tw-col-span-12 md:tw-col-span-12 sm:tw-col-span-12 xs-tw-col-span-12  tw-mt-7"]')
+            .children('h1[class="sui-font-heading sui-text-dh2 sui-text-darkGray-darker tw-font-bold"]')
+            .should('have.text', 'Welcome back!')
+        })
+    })
+
+
     describe('Authenticate User', () => {
-        it('Enter Username', () => {
+        it('Enter Username', function () {
             cy.get('input[name="email"]')
-            .type('zronsing@gmail.com')
+            .type(this.credentials.username)
             .should('have.value', 'zronsing@gmail.com')
         })
 
@@ -15,31 +49,35 @@ context('Update User Profile', () => {
             .click()
         })
 
-        it('Enter Password', () => {
+        it('Enter Password', function () {
             cy.get('input[name="password"]')
-            .type('Welcome06#')
+            .type(this.credentials.password)
         })
 
         it('Click Sign-in Button', () => {
             cy.get('button[type=submit]')
             .click()
+
+            cy.wait(5000)
         })
     })
 
     describe('Update Basic Information', () => {
         describe('Navigate to My Profile Page' , () => {
             it('Click Profile Icon', () => {
-                cy.wait(5000).get('button[class="sui-flex sui-items-center focus:sui-outline-none"]').click()
+                cy.get('button[class="sui-flex sui-items-center focus:sui-outline-none"]').click()
             })
 
             it('Click My Profile', () => {
                 cy.get('span[class="sui-ml-sm"]')
                 .contains('My Profile')
                 .click()
+
+                cy.wait(3000)
             })
 
-            it('Validate Profile Page URL', () => {
-                cy.wait(5000).url().should('eq', 'https://www.telusinternational.ai/cmp/contributor/userprofile/basic-info')
+            it('Validate Profile Page URL', function () {
+                cy.url().should('eq', this.url.profileURL)
             })
         })
 
@@ -47,57 +85,54 @@ context('Update User Profile', () => {
 
 
             it('Click Contact Info Edit Button', () => {
-                cy.wait(5000)
-                .get('div[class="contact-info border-top tw-mb-6 ml-0"]')
+                cy.get('div[class="contact-info border-top tw-mb-6 ml-0"]')
                 .children('div[class="row figma-section-header-text-margin"]')
                 .children('div[class="col-lg-2 col-md-2"]')
-                .children('button[type=button]')
-                .click()
+                .children('button[type=button]').click()
+
+                cy.wait(1000)
             })
 
-            it('Validate Save Button is Disabled', () => {
-                cy.get('button[aria-disabled="true"]')
-                .should('be.disabled')
+            it('Validate Contact Info Save Button is Disabled', () => {
+                cy.get('button[aria-disabled="true"]').should('be.disabled')
             })
 
-            it('Update Firstname', () => {
+            it('Update Firstname', function () {
                 cy.get('input[name="first_name"]').then(($input) => {
                     const first_name = $input.val()
 
                     if (first_name == 'Zul') {
                         cy.get('input[name="first_name"]').clear()
                         cy.get('input[name="first_name"]')
-                        .type('Zul-Qarnain')
+                        .type(this.userProfile.contact.firstName)
                     } else {
                         cy.get('input[name="first_name"]').clear()
                         cy.get('input[name="first_name"]')
-                        .type('Zul')
+                        .type(this.userProfile.contact.nickName)
                     }
                 })
             })
 
-            it('Update Middlename', () => {
+            it('Update Middlename', function () {
                 cy.get('input[name="middle_name"]').then(($input) => {
                     const middle_name = $input.val()
-
-                    cy.log("Middlename: " +middle_name)
 
                     cy.get('input[name="middle_name"]').clear()
                     switch (middle_name) {
                         case '':
                         case 'A.':
                             cy.get('input[name="middle_name"]')
-                            .type('Ayo')
+                            .type(this.userProfile.contact.middleName)
                             break;
                         default:
                             cy.get('input[name="middle_name"]')
-                            .type('A.')
+                            .type(this.userProfile.contact.middleInitial)
                             break;
                     }
                 })
             })
 
-            it('Update Contact Number Value', () => {
+            it('Update Contact Number Value', function () {
                 cy.get('input[name="phone_number.line_number"]').then(($input) => {
                     const phone_number = $input.val()
 
@@ -106,37 +141,195 @@ context('Update User Profile', () => {
                         case '':
                         case '9190627118':
                             cy.get('input[name="phone_number.line_number"]')
-                            .type('9770188363')
+                            .type(this.userProfile.contact.phoneNumber2)
                             break;
                         default:
                             cy.get('input[name="phone_number.line_number"]')
-                            .type('9190627118')
+                            .type(this.userProfile.contact.phoneNumber1)
                             break;
                     }
                 })
+
+                cy.wait(1000)
             })
 
-            it('Validate Save Button is Enabled', () => {
-                cy.get('button[aria-disabled="false"]')
-                .should('be.enabled')
+            it('Validate Contact Info Save Button is Enabled', () => {
+                cy.get('button[aria-disabled="false"]').should('be.enabled')
             })
 
-            it('Click Save Button', () => {
-                cy.get('button[aria-disabled="false"]')
-                .click()
+            it('Click Contact Info Save Button', () => {
+                cy.get('button[aria-disabled="false"]').click()
+
+                cy.wait(3000)
             })
         })
 
         describe('Update Location', () => {
             it('Click Location Edit Button', () => {
-                cy.wait(5000)
-                .get('div[class="location border-top border-top"]')
+                cy.get('div[class="location border-top border-top"]')
                 .children('div[class]')
                 .children('div[class="row mt-6"]')
                 .children('div[class="col-lg-2 col-md-2"]')
-                .children('button[type=button]')
-                .click()
+                .children('button[type=button]').click()
+
+                cy.wait(1000)
             })
+
+            it('Validate Location Save Button is Disabled', () => {
+                cy.get('button[aria-disabled="true"]').should('be.disabled')
+            })
+
+            it('Update Street Value', function () {
+                cy.get('input[name="streetAddress"]').then(($input) => {
+                    const street = $input.val()
+
+                    cy.get('input[name="streetAddress"]').clear()
+                    switch (street) {
+                        case '':
+                        case 'EDSA corner Madison Street':
+                            cy.get('input[name="streetAddress"]').type(this.userProfile.location.street1)
+                            break;
+                        default:
+                            cy.get('input[name="streetAddress"]').type(this.userProfile.location.street2)
+                            break
+                    }
+                })
+            })
+
+            it('Update City and State Value', function () {
+                cy.get('input[name="cityAndState"]').then(($input) => {
+                    const cityAndState = $input.val()
+
+                    cy.get('input[name="cityAndState"]').clear()
+                    switch (cityAndState) {
+                        case '':
+                        case 'Mandaluyong City, National Capital Region':
+                            cy.get('input[name="cityAndState"]').type(this.userProfile.location.cityAndState1)
+                            break;
+                        default:
+                            cy.get('input[name="cityAndState"]').type(this.userProfile.location.cityAndState2)
+                            break;
+                    }
+                    cy.wait(6000)
+                    cy.get('ul li:first').click()
+                    cy.wait(3000)
+                })
+            })
+
+            it('Update Postal Code Value', function () {
+                cy.get('input[name="postalCode"]').then(($input) => {
+                    const postalCode = $input.val()
+
+                    cy.get('input[name="postalCode"]').clear()
+                    switch (postalCode) {
+                        case '':
+                        case '1550':
+                            cy.get('input[name="postalCode"]').type(this.userProfile.location.postalCode1)
+                            break;
+                        default:
+                            cy.get('input[name="postalCode"]').type(this.userProfile.location.postalCode2)
+                            break;
+                    }
+                })
+
+                cy.wait(1000)
+            })
+
+            it('Validate Location Save Button is Enabled', () => {
+                cy.get('button[aria-disabled="false"]').should('be.enabled')
+            })
+
+            it('Click Location Save Button', () => {
+                cy.get('button[aria-disabled="false"]').click()
+
+                cy.wait(3000)
+            })
+
+            it('Validate Successfully Saved Notification Appeared', () => {
+                cy.get('div[class="sui-flex sui-gap-3 tw-p-3"]')
+                .children('div')
+                .children('p[class="suit-text-h4 sui-text-darkGray-darker tw-font-bold"]')
+                .contains('Success')
+                .should('be.visible')
+
+                cy.get('div[class="sui-flex sui-gap-3 tw-p-3"]')
+                .children('div')
+                .children('p[class="tw-font-normal suit-text-h5 sui-text-darkGray-darker sui-pt-sm"]')
+                .contains('Location successfully saved!')
+                .should('be.visible')
+            })
+
+        })
+    })
+
+    describe('Update Languages', () => {
+        describe('Navigate to Languages Page', () => {
+            it('Click Languages Left Navigation Menu', () => {
+                cy.get('a[href="/cmp/contributor/userprofile/languages"]').click()
+
+                cy.wait(3000)
+            })
+
+            it('Validate Language Page URL', function () {
+                cy.url().should('eq', this.url.languageURL)
+            })
+        })
+
+        describe('Add Other Languages', () => {
+            it('Click Other Languages Add Button', () => {
+                cy.get('div[class="row figma-section-header-text-margin"]')
+                .children('div[class="col-lg-2 col-md-2"]')
+                .children('button').click()
+
+                cy.wait(1000)
+            })
+
+            it('Select Language', () => {
+                cy.get('input[aria-label="Type language and select*"]')
+                .type('Chiga (Uganda){enter}')
+            })
+
+            it('Select Proficiency Level', () => {
+                cy.get('input[aria-label="Select proficiency level*"]')
+                .type('Native or bilingual proficiency{enter}')
+            })
+
+            it('Click Add Language Save Button', () => {
+                cy.get('button[aria-disabled="false"]').click()
+
+                cy.wait(3000)
+            })
+        })
+
+        describe('Remove Other Languages', () => {
+            it('Click Delete Button', () => {
+                cy.get('div[class="wrapper-update-section tw-pt-3"]')
+                .children('div[class="trash-div"]')
+                .children('svg[class="svg-inline--fa fa-trash-alt fa-w-14"]')
+                .click()
+
+                cy.wait(1000)
+            })
+
+            it('Confirm Delete Other Language', () => {
+                cy.get('button[type=submit]').click()
+
+                cy.wait(3000)
+            })
+        })
+    })
+
+    describe('Sign-out from the Application', () => {
+        it('Click Profile Icon', () => {
+            cy.get('button[class="sui-flex sui-items-center focus:sui-outline-none"]').click()
+        })
+
+        it('Click Sign-out', () => {
+            cy.get('span[class="sui-ml-sm"]')
+            .contains('Sign Out')
+            .click()
+
+            cy.wait(3000)
         })
     })
 })
